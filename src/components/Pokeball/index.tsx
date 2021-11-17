b98182d80f7be2681f4722062c645429f406c470
Leonardo <56121325+Leonnard19@users.noreply.github.com>
2021-11-17 17:04:00 -0300

    pokedesk - more improvements

--- src/components/Pokeball/index.tsx
+++ src/components/Pokeball/index.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import { useSelectedPokemons } from '../../context/SelectedPokemonsContext';
+import Image from "next/image";
+import { useSelectedPokemons } from "../../context/SelectedPokemonsContext";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 type TypePokemon = {
   image: string;
@@ -13,19 +13,52 @@ export function Pokeball({ pokemon }: { pokemon?: TypePokemon }) {
   const { isEditing, setIsEditing } = useSelectedPokemons();
 
   function editPokemon() {
+    if (window.location.href.includes("teams")) {
+      return;
+    }
     isEditing ? setIsEditing(undefined) : setIsEditing(pokemon?.id);
-    console.log(pokemon?.name); // testing the selection
   }
 
   return (
-    <div className={styles.container} onClick={editPokemon}>
+    <div
+      className={styles.container}
+      onClick={editPokemon}
+      style={
+        !window.location.href.includes("teams") &&
+        isEditing &&
+        isEditing !== pokemon?.id
+          ? {
+              filter: "brightness(80%) grayscale(70%)",
+            }
+          : {}
+      }
+    >
       <div>
         <Image src="/assets/Pokeball1.svg" alt="img" width={65} height={65} />
       </div>
 
       {pokemon && (
-        <div className={styles.pokemonImg}>
-          {<Image src={pokemon.image} alt="img" width={70} height={70} unoptimized />}
+        <div
+          className={styles.pokemonImg}
+          style={
+            !window.location.href.includes("teams") &&
+            isEditing &&
+            isEditing === pokemon?.id
+              ? {
+                  filter: `drop-shadow(2px 4px 6px)`,
+                }
+              : {}
+          }
+        >
+          {
+            <Image
+              src={pokemon.image}
+              alt="img"
+              width={70}
+              height={70}
+              unoptimized
+            />
+          }
         </div>
       )}
     </div>
--- src/components/Teams/index.tsx
+++ src/components/Teams/index.tsx
@@ -1,19 +1,35 @@
-import styles from './styles.module.scss';
-import Image from 'next/image';
-import { Pokeball } from '../Pokeball';
-import { useState } from 'react';
-import { useSelectedPokemons } from '../../context/SelectedPokemonsContext';
-import { useTeamsContext } from '../../context/TeamsContext';
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import { Pokeball } from "../Pokeball";
+import { useState } from "react";
+import { useSelectedPokemons } from "../../context/SelectedPokemonsContext";
+import { useTeamsContext } from "../../context/TeamsContext";
 
 export function Teams() {
-  const [text, setText] = useState('My Team');
-  const { selectedPokemons, setSelectedPokemons } = useSelectedPokemons();
+  const [text, setText] = useState("My Team");
+  const { selectedPokemons, setSelectedPokemons, isEditing, setIsEditing } =
+    useSelectedPokemons();
   const { addTeam } = useTeamsContext();
+
+  function removePokemon() {
+    const pokemon = selectedPokemons.find((pokemon) => {
+      return pokemon.id === isEditing;
+    });
+
+    if (pokemon) {
+      const newSelectedPokemons = selectedPokemons.filter(
+        (pokemon) => pokemon.id !== isEditing
+      );
+
+      setSelectedPokemons(newSelectedPokemons);
+      setIsEditing(undefined);
+    }
+  }
 
   return (
     <>
       <div className={styles.title}>
-        <input onChange={e => setText(e.target.value)} value={text} />
+        <input onChange={(e) => setText(e.target.value)} value={text} />
         <div>
           <Image src="/assets/Vector.svg" alt="edit" width={10} height={10} />
         </div>
@@ -31,8 +47,13 @@ export function Teams() {
       </div>
 
       <div className={styles.buttonContainer}>
-        <button disabled>
-          <Image src="/assets/DeleteButton.svg" alt="delete" width={40} height={40} />
+        <button disabled={!isEditing} onClick={removePokemon}>
+          <Image
+            src="/assets/DeleteButton.svg"
+            alt="delete"
+            width={40}
+            height={40}
+          />
         </button>
         <button
           onClick={() => addTeam({ title: text, pokemons: selectedPokemons })}
